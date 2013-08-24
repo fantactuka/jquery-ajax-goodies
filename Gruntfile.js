95abd933e034d524ab9a7345125f396f511c9b2d
Maksim Horbachevsky <fantactuka@gmail.com>
2013-08-24 11:02:41 +0300

    Adding release task

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,13 +39,24 @@ module.exports = function(grunt) {
       options: {
         jshintrc: '.jshintrc'
       }
+    },
+    version: {
+      update: {
+        src: ['component.json', 'package.json']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-version');
 
   grunt.registerTask('test', ['jshint', 'karma:ci']);
   grunt.registerTask('default', ['test', 'uglify']);
+
+  grunt.registerTask('release', 'Releasing new version with update version', function() {
+    var type = this.args[0] || 'patch';
+    grunt.task.run(['test', 'version:update:' + type, 'uglify']);
+  });
 };
